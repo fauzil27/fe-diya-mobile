3e989bde46ee1df2e28ad95bd0d1627b6f3b7703
fauzil27 <fauziladhim706@gmail.com>
2024-08-13 19:24:51 +0800

    input

--- components/InputCstm.tsx
+++ components/InputCstm.tsx
@@ -8,6 +8,7 @@ export default function InputCstm({
   focus?: boolean;
 }) {
   const [focus, setfocus] = useState(false);
+
   const [text, setText] = useState("");
 
   return (
@@ -17,7 +18,7 @@ export default function InputCstm({
           focus ? "border-2 border-[#08244B]" : ""
         }  rounded-xl`}
       >
-        {focus && text !== "" ? null : (
+        {text !== "" ? null : (
           <Text className="absolute text-lg top-2 left-2 text-stone-400">
             {placeholder}
           </Text>
@@ -25,7 +26,8 @@ export default function InputCstm({
         <View onPointerDown={() => setfocus(true)} className="px-2 py-2 w-full">
           <TextInput
             className="text-lg w-full"
-            onPressIn={() => setfocus(true)}
+            onFocus={() => setfocus(true)}
+            onBlur={() => setfocus(false)}
             onChangeText={(e) => setText(e)}
           />
         </View>
